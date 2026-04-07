4c1c78c3de9d4632b1350e2e975fb47587e4c073
manW13-be <manW13-be@proton.me>
2026-04-07 07:01:03 +0200

    fix: remove venv from repo

--- venv/lib/python3.12/site-packages/PyTado/interface/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Abstraction layer for API implementation."""
-
-from .interface import Tado
-
-__all__ = ["Tado"]
--- venv/lib/python3.12/site-packages/PyTado/zone/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Zone data structures for all API interfaces."""
-
-from .hops_zone import TadoXZone
-from .my_zone import TadoZone
-
-__all__ = ["TadoZone", "TadoXZone"]
